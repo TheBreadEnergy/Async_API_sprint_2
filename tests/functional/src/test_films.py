717c54743548eb7f6142d452615a32f3f2afdabe
Samir Akhmed <warrior.owl@yandex.ru>
2023-12-05 17:54:08 +0300

    Fix test_pagination

--- tests/functional/src/test_films.py
+++ tests/functional/src/test_films.py
@@ -12,8 +12,10 @@ from functional.testdata.movie_template import MOVIE_TEMPLATE
 
 @pytest.mark.parametrize("query_data, expected_answer", MOVIE_PAGINATION_TEST_QUERY)
 @pytest.mark.asyncio
-async def test_pagination(make_get_request, query_data: dict, expected_answer: dict):
-
+async def test_pagination(
+    es_write_data, make_get_request, query_data: dict, expected_answer: dict
+):
+    await es_write_data(test_settings.es_movie_index, MOVIE_TEMPLATE, MOVIE_MOCK_DATA)
     response = await make_get_request("/api/v1/films/", query_data)
     assert response["status"] == expected_answer["status"]
     assert len(response["body"]) == expected_answer["length"]
@@ -27,7 +29,6 @@ async def test_sort(
     await es_write_data(test_settings.es_movie_index, MOVIE_TEMPLATE, MOVIE_MOCK_DATA)
     response = await make_get_request("/api/v1/films/", query_data)
     assert response["status"] == expected_answer["status"]
-
     assert [item["id"] for item in response["body"]] == expected_answer["body"]
 
 
